31955c068fd1f77d8278acf242cc9e021527a556
Zenitude <mollet.rudy@gmail.com>
2022-03-08 16:02:15 +0100

    avancement sidebar

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ catch (Exception $e)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="ressources/logo.png" />
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/index.css">
     <title>Bustronome | Restaurant grastronomique original à Paris & ... </title>
--- pages/indexParis.php
+++ pages/indexParis.php
@@ -15,6 +15,8 @@ catch (Exception $e)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="../ressources/logo.png" />
+    <script src="https://kit.fontawesome.com/10a6ffe905.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/main.css">
     <link rel="stylesheet" href="../styles/indexParis.css">
     <title>Paris | Bustronome | Restaurant grastronomique original à Paris & ... </title>
@@ -22,35 +24,49 @@ catch (Exception $e)
 <body>
     <div class="container">
         <div class="slider">
-            <div id="sidebar">
-                <a href="../index.php"><img src="../ressources/logo.png" alt="lien vers accueil"></a>
-                <ul>
-                <li><a href="">Réservez Maintenant</a></li>
-                <li><a href="">Carte Cadeau</a></li>
-                <li><a href="">Les offres</a></li>
-                <li><a href="">Le concept</a></li>
-                <li><a href="">Privatisation</a></li>
-                <li><a href="">Actualités du bus</a></li>
-                <li><a href="">Le blog</a></li>
-                <li><a href="">Photos et vidéos</a></li>
-                <li><a href="">Questions fréquentes</a></li>
-                <li><a href="">Contactez le bustronome Paris</a></li>
-                </ul>
-                <p>
+            <div class="sidebar">
+                <nav>
+                    <a href="../index.php" id="logo"><img src="../ressources/logo.png" alt="lien vers accueil"></a>
+                    <ul>
+                        <li><a href="">Réservez Maintenant</a></li>
+                        <li><a href="">Carte Cadeau</a></li>
+                        <li><a href="">Les offres</a></li>
+                        <li><a href="">Le concept</a></li>
+                        <li><a href="">Privatisation</a></li>
+                        <li><a href="">Actualités du bus</a></li>
+                        <li><a href="">Le blog</a></li>
+                        <li><a href="">Photos et vidéos</a></li>
+                        <li><a href="">Questions fréquentes</a></li>
+                        <li><a href="">Contactez le bustronome Paris</a></li>
+                    </ul>
+                </nav>
+                <p class="geoloc">
                     <img src="" alt="">
                     <p>Arc de triomphe</p>
                     <a href="https://www.google.com/maps/place/Bustronome+-+Paris/@48.872752,2.294031,15z/data=!4m5!3m4!1s0x0:0x2b80095c2d0654ca!8m2!3d48.872752!4d2.294031">angle avenue Kleber</a>
                 </p>
             </div>
+            <form>
+                <select name="selectCity" id="selectCity">
+                    <option value="paris">Paris</option>
+                    <option value="londres">Londres</option>
+                </select>
+            </form>
             <div class="barDefil"></div>
         </div>
         <footer>
-            <button>FR</button> | <button>EN</button> 
-            <span class="tel"> <i class="fa-solid fa-phone"></i> +33 (0) 954 444 555</span>
-            <a href="mailto:contact@bustronome.com"><i class="fa-regular fa-envelope">contact@bustronome.com</i></a>
-            <a href=""><i class="fa-brands fa-facebook-f"></i></a>
-            <a href=""><i class="fa-brands fa-google-plus-g"></i></a>
-            <a href=""><i class="fa-brands fa-instagram-square"></i></a>
+            <div class="contactReseaux">
+                <button>FR</button> | <button>EN</button> 
+                <i class="fa-solid fa-phone"></i> <span class="tel">+33 (0) 954 444 555</span>
+                <i class="fa-regular fa-envelope"><a href="mailto:contact@bustronome.com">contact@bustronome.com</i></a>
+                <a href=""><i class="fa-brands fa-facebook-f" id="fb"></i></a>
+                <a href=""><i class="fa-brands fa-google-plus-g" id="gg"></i></a>
+                <a href=""><i class="fa-brands fa-instagram-square" id="insta"></i></a>
+            </div>
+            <div class="copyMadeIn">
+               <p>&copy; BUSTRONOME. <a href="legalnotice.php"><span>LEGAL NOTICES</span></a> DEVELOPED BY</p> 
+               <p>Made with <i class="fa-solid fa-heart"></i> by CosaVostra</p> 
+            </div>
          </footer>
     </div>
     
